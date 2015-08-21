f96abf2680c86f41433f5c608faf726b6ced0ea4
Evgenii Bondarev <putnik@ukr.net>
2015-08-21 13:11:05 +0300

    First implementation of the General Sadhana table view cell UI

--- Vaishnavaseva/ViewControllers/GeneralSadhanaViewController.swift
+++ Vaishnavaseva/ViewControllers/GeneralSadhanaViewController.swift
@@ -1,7 +1,9 @@
 import UIKit
 
-class GeneralSadhanaViewController: BaseViewController {
+class GeneralSadhanaViewController: BaseViewController, UITableViewDelegate, UITableViewDataSource {
 
+  @IBOutlet var tableView: UITableView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -33,6 +35,21 @@ class GeneralSadhanaViewController: BaseViewController {
   @IBAction func backToGeneralSadhana(segue:UIStoryboardSegue)
     {
     }
-
+  
+  func numberOfSectionsInTableView(tableView: UITableView) -> Int
+    {
+    return 1
+    }
+  
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+    return 1
+    }
+  
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+    let cell = tableView.dequeueReusableCellWithIdentifier("PersonalCell", forIndexPath: indexPath) as UITableViewCell
+    return cell
+    }
 }
 
