9b55306bbff8e798b03154d1c09c1add821969d8
Roman Slysh <monar.ua@gmail.com>
2015-09-02 16:44:09 +0300

    LoginViewController simple implementation (need to design)

--- Vaishnavaseva/ViewControllers/LogInViewController.swift
+++ Vaishnavaseva/ViewControllers/LogInViewController.swift
@@ -3,6 +3,8 @@ import UIKit
 class LogInViewController: BaseViewController {
 
   @IBOutlet weak var japaView: JapaView!
+  @IBOutlet weak var loginTextField: UITextField!
+  @IBOutlet weak var passwordTextField: UITextField!
   
 
   @IBAction func onButtonPressed(sender: AnyObject)
@@ -16,6 +18,8 @@ class LogInViewController: BaseViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    loginTextField.text = "roman"
+    passwordTextField.text = "Ubuntu108"
     // Do any additional setup after loading the view, typically from a nib.
   }
   
@@ -41,14 +45,61 @@ class LogInViewController: BaseViewController {
 
   @IBAction func onLogIn(sender: AnyObject)
     {
-    AppController.sharedAppController.isLoggedIn = true
-    performSegueWithIdentifier("LogInToMy", sender: nil)
+    if login(loginTextField.text!, password: passwordTextField.text!)
+    {
+        AppController.sharedAppController.isLoggedIn = true
+        performSegueWithIdentifier("LogInToMy", sender: nil)
+    } else
+    {
+        let alert = UIAlertController(title: "Authorization error!", message: "", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+        passwordTextField.text = ""
+        passwordTextField.becomeFirstResponder()
+    }
     }
   
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+    
+    func login(login: String, password: String) -> Bool
+    {
+        var loginSuccess = false
+        let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+        let userPasswordString = login + ":" + password
+        let userPasswordData = userPasswordString.dataUsingEncoding(NSUTF8StringEncoding)
+        let base64EncodedCredential = userPasswordData!.base64EncodedStringWithOptions(NSDataBase64EncodingOptions.Encoding64CharacterLineLength)
+        let authString = "Basic \(base64EncodedCredential)"
+        config.HTTPAdditionalHeaders = ["Authorization" : authString]
+        let session = NSURLSession(configuration: config)
+        
+        var running = false
+        let url = NSURL(string: "http://vaishnavaseva.net/vs-api/v1/sadhana/me")
+        var dataString: NSString = ""
+        let task = session.dataTaskWithURL(url!) {
+            (let data, let response, let error) in
+            if let _ = response as? NSHTTPURLResponse {
+                dataString = NSString(data: data!, encoding: NSUTF8StringEncoding)!
+                if ((response as? NSHTTPURLResponse)!.statusCode == 200)
+                {
+                    loginSuccess = true
+                }
+                print(dataString)
+            }
+            running = false
+        }
+        
+        running = true
+        task.resume()
+        
+        while running {
+            print("waiting...")
+            sleep(1)
+        }
+        return loginSuccess
+    }
 
 }
 
