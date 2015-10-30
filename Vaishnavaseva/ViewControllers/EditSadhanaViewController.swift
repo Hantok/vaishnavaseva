03fc56e81c3a593d0530bc9c9102773a24383e2f
Evgenii Bondarev <putnik@ukr.net>
2015-10-30 17:56:11 +0200

    First implementation of animated pickers for japa rounds and wake/sleep time

--- Vaishnavaseva/ViewControllers/EditSadhanaViewController.swift
+++ Vaishnavaseva/ViewControllers/EditSadhanaViewController.swift
@@ -1,19 +1,37 @@
 import UIKit
 
-enum sadhanaType: Int {
-  case Books, Kirtan, Japa, Wake, Sleep, Service, Joga, Lectures, Count
+let pickerHeightMinimized: CGFloat = 36
+let pickerHeightMaximized: CGFloat = 216//may be took from the picker view itself
+
+struct SadhanaTypesEnableState {
+  var Wake = true
+  var Sleep = true
+  var Service = true
+  var Joga = true
+  var Lectures = true
 }
 
 let OnDoneStateViewEvent = "OnDoneStateViewEvent"
 
-class EditSadhanaViewController: BaseTableViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+class EditSadhanaViewController: BaseViewController, UIPickerViewDataSource, UIPickerViewDelegate, HitTestDelegate {
   
-  //  @IBOutlet weak var japaBooksPicker: UIPickerView!
-  //  @IBOutlet weak var serviceSwitch: UISwitch!
-  //  @IBOutlet weak var kirtanSwitch: UISwitch!
-  //  @IBOutlet weak var lecturesSwitch: UISwitch!
-  //  @IBOutlet weak var wakeupTimePicker: UIDatePicker!
-  //  @IBOutlet weak var bedtimePicker: UIDatePicker!
+  @IBOutlet weak var booksMinutesTextField: UITextField!
+  @IBOutlet weak var booksMinutesStepperControl: UIStepper!
+  @IBOutlet weak var kirtanSwitch: UISwitch!
+  @IBOutlet weak var serviceSwitch: UISwitch!
+  @IBOutlet weak var jogaSwitch: UISwitch!
+  @IBOutlet weak var lecturesSwitch: UISwitch!
+  @IBOutlet weak var japaRoundsPickerView: HitTestPicker!
+  @IBOutlet weak var japaRoundsPickerSuperviewHeightConstraint: NSLayoutConstraint!
+  @IBOutlet weak var japaRoundsPickerSuperviewTopConstraint: NSLayoutConstraint!
+  @IBOutlet weak var wakeTimePickerView: HitTestDatePicker!
+  @IBOutlet weak var wakeTimePickerSuperviewHeightConstraint: NSLayoutConstraint!
+  @IBOutlet weak var wakeTimePickerSuperviewTopConstraint: NSLayoutConstraint!
+  @IBOutlet weak var sleepTimePickerView: HitTestDatePicker!
+  @IBOutlet weak var sleepTimePickerSuperviewHeightConstraint: NSLayoutConstraint!
+  @IBOutlet weak var sleepTimePickerSuperviewTopConstraint: NSLayoutConstraint!
+  var superviewHeightConstraints: [NSLayoutConstraint] = []
+  var superviewTopConstraints: [NSLayoutConstraint] = []
   
   var booksReadInMinutes = 0
   var kirtanDone = false
@@ -21,113 +39,147 @@ class EditSadhanaViewController: BaseTableViewController, UIPickerViewDataSource
   var jogaDone = false
   var lecturesDone = false
   
-  var sadhanaTypesActive: [Bool] = Array(count: sadhanaType.Count.rawValue, repeatedValue: true)
-  var activeRows: [Int]!
+  var sadhanaTypesEnableState = SadhanaTypesEnableState()
+  
+  var animationsEnabled = false
+  var lastAnimatedView: UIView?
+  var minimizationTimer: NSTimer?
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    //Apply active sadhana types from user settings here:
-    //sadhanaTypesActive[sadhanaTypesOrder.indexOf("sleep")!] = false
     
-    activeRows = []
-    for (row, active) in sadhanaTypesActive.enumerate() {
-      if active {
-        activeRows.append(row)
-      }
-    }
+    japaRoundsPickerView.hitTestDelegate = self
+    wakeTimePickerView.hitTestDelegate = self
+    sleepTimePickerView.hitTestDelegate = self
+    
+    japaRoundsPickerView.tag = 0
+    wakeTimePickerView.tag = 1
+    sleepTimePickerView.tag = 2
+    
+    superviewHeightConstraints = [
+      japaRoundsPickerSuperviewHeightConstraint,
+      wakeTimePickerSuperviewHeightConstraint,
+      sleepTimePickerSuperviewHeightConstraint]
+    superviewTopConstraints = [
+      japaRoundsPickerSuperviewTopConstraint,
+      wakeTimePickerSuperviewTopConstraint,
+      sleepTimePickerSuperviewTopConstraint]
+    
+    //Apply active sadhana types from user settings here like next:
+    sadhanaTypesEnableState.Sleep = false
+    sadhanaTypesEnableState.Joga = false
+    
+    wakeTimePickerView.userInteractionEnabled = sadhanaTypesEnableState.Wake
+    sleepTimePickerView.userInteractionEnabled = sadhanaTypesEnableState.Sleep
+    serviceSwitch.enabled = sadhanaTypesEnableState.Service
+    jogaSwitch.enabled = sadhanaTypesEnableState.Joga
+    lecturesSwitch.enabled = sadhanaTypesEnableState.Lectures
+    
+    kirtanSwitch.setOn(kirtanDone, animated: false)
+    booksMinutesTextField.text = booksReadInMinutes > 0 ? String("\(booksReadInMinutes)") : ""
+    booksMinutesStepperControl.value = Double(booksReadInMinutes)
+    serviceSwitch.setOn(serviceDone, animated: false)
+    jogaSwitch.setOn(jogaDone, animated: false)
+    lecturesSwitch.setOn(lecturesDone, animated: false)
+  }
+  
+  override func viewDidAppear(animated: Bool) {
+    animationsEnabled = true
   }
   
   @IBAction func onDone(sender: AnyObject) {
     sendActionForStateViewEvent(OnDoneStateViewEvent)
   }
   
-  
-  override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-    return 1
+  @IBAction func onStepperValueChanged(stepper: UIStepper) {
+    booksMinutesTextField.text = String("\(Int(stepper.value))")
   }
   
-  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return activeRows.count
-  }
-  
-  //override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-    
-  //}
-  
-  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-    let row = activeRows[indexPath.row]
-    switch row {
-    case sadhanaType.Books.rawValue:
-      let cell = tableView.dequeueReusableCellWithIdentifier("EditCountSadhanaTableViewCell") as! EditCountSadhanaTableViewCell
-      cell.label.text = NSLocalizedString("Books, min", comment: "Books label in edit sadhana")
-      cell.countTextField.text = booksReadInMinutes > 0 ? String("\(booksReadInMinutes)") : ""
-      cell.stepperControl.value = Double(booksReadInMinutes)
-      return cell
-    case sadhanaType.Kirtan.rawValue:
-      let cell = tableView.dequeueReusableCellWithIdentifier("EditBoolSadhanaTableViewCell") as! EditBoolSadhanaTableViewCell
-      cell.label.text = NSLocalizedString("Kirtan", comment: "Kirtan label in edit sadhana")
-      cell.switchControl.setOn(kirtanDone, animated: false)
-      return cell
-    case sadhanaType.Japa.rawValue:
-      let cell = tableView.dequeueReusableCellWithIdentifier("EditBoolSadhanaTableViewCell") as! EditBoolSadhanaTableViewCell
-      cell.label.text = NSLocalizedString("Kirtan", comment: "Kirtan label in edit sadhana")
-      cell.switchControl.setOn(kirtanDone, animated: false)
-      return cell
-    case sadhanaType.Wake.rawValue:
-      let cell = tableView.dequeueReusableCellWithIdentifier("EditBoolSadhanaTableViewCell") as! EditBoolSadhanaTableViewCell
-      cell.label.text = NSLocalizedString("Kirtan", comment: "Kirtan label in edit sadhana")
-      cell.switchControl.setOn(kirtanDone, animated: false)
-      return cell
-    case sadhanaType.Sleep.rawValue:
-      let cell = tableView.dequeueReusableCellWithIdentifier("EditBoolSadhanaTableViewCell") as! EditBoolSadhanaTableViewCell
-      cell.label.text = NSLocalizedString("Kirtan", comment: "Kirtan label in edit sadhana")
-      cell.switchControl.setOn(kirtanDone, animated: false)
-      return cell
-    case sadhanaType.Service.rawValue:
-      let cell = tableView.dequeueReusableCellWithIdentifier("EditBoolSadhanaTableViewCell") as! EditBoolSadhanaTableViewCell
-      cell.label.text = NSLocalizedString("Service", comment: "Service label in edit sadhana")
-      cell.switchControl.setOn(kirtanDone, animated: false)
-      return cell
-    case sadhanaType.Joga.rawValue:
-      let cell = tableView.dequeueReusableCellWithIdentifier("EditBoolSadhanaTableViewCell") as! EditBoolSadhanaTableViewCell
-      cell.label.text = NSLocalizedString("Joga", comment: "Joga label in edit sadhana")
-      cell.switchControl.setOn(kirtanDone, animated: false)
-      return cell
-    default://lectures
-      let cell = tableView.dequeueReusableCellWithIdentifier("EditBoolSadhanaTableViewCell") as! EditBoolSadhanaTableViewCell
-      cell.label.text = NSLocalizedString("Lectures", comment: "Lectures label in edit sadhana")
-      cell.switchControl.setOn(kirtanDone, animated: false)
-      return cell
+  //func textViewDidChange(_ textView: UITextView)
+  @IBAction func onCountTextFieldEditingChanged(textField: UITextField) {
+    if let value = Double((textField.text)!) {
+      booksMinutesStepperControl.value = value
+    } else {
+      booksMinutesStepperControl.value = 0
     }
   }
   
   func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
-    return 5
+    return 4
   }
   
   func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-    if component == 4 {
-      return 24*4//15 minutes interval is 1/4 of an hour
-    } else {
-      return 65
-    }
+    return 65
   }
   
   func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-    if component == 4 {
-      return String(row*15)
-    } else {
-      return String(row)
-    }
+    return String(row)
   }
   
   func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+    self.minimizeViewAnimatedIfNeeded()
   }
   
+  @IBAction func onWakeTimeChanged(sender: UIDatePicker) {
+    self.minimizeViewAnimatedIfNeeded()
+  }
+  
+  @IBAction func onSleapDateChanged(sender: UIDatePicker) {
+    self.minimizeViewAnimatedIfNeeded()
+  }
+  
+  func minimizeViewAnimatedIfNeeded() {
+    if let lastAnimated = lastAnimatedView {
+      minimizeViewAnimated(lastAnimated)
+      lastAnimatedView = nil
+    }
+  }
+  
+  func hitTestCalledForView(view: UIView){
+    if animationsEnabled  && lastAnimatedView != view {
+      if let lastAnimated = lastAnimatedView {
+        minimizeViewAnimated(lastAnimated)
+      }
+      lastAnimatedView = view
+      maximizeViewAnimated(view)
+      minimizationTimer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "minimizeViewAnimatedIfNeeded", userInfo: nil, repeats: false)
+    }
+  }
+  
+  func maximizeView(view: UIView) {
+    self.view.bringSubviewToFront(view.superview!)
+    superviewTopConstraints[view.tag].constant -= (pickerHeightMaximized - pickerHeightMinimized)/2.0
+    superviewHeightConstraints[view.tag].constant = pickerHeightMaximized
+    self.view.layoutIfNeeded()
+    self.view.bringSubviewToFront(view.superview!)
+  }
+  
+  func maximizeViewAnimated(view: UIView){
+    UIView.animateWithDuration(0.5, delay: 0, options: [.AllowUserInteraction], animations: {self.maximizeView(view)}, completion: nil)
+    //    UIView.animateWithDuration(0.3) {//[.AllowUserInteraction] is needed here!!!
+    //      self.maximizeView(view)
+    //    }
+  }
+  
+  func minimizeView(view: UIView) {
+    superviewTopConstraints[view.tag].constant += (pickerHeightMaximized - pickerHeightMinimized)/2.0
+    superviewHeightConstraints[view.tag].constant = pickerHeightMinimized
+    self.view.layoutIfNeeded()
+  }
+  
+  func minimizeViewAnimated(view: UIView){
+    UIView.animateWithDuration(0.3) {
+      self.minimizeView(view)
+    }
+  }
   
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
+  }
+  
+  deinit {
+    minimizationTimer?.invalidate()
   }
   
 }
---
+++ Vaishnavaseva/Views/HitTestDatePicker.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+class HitTestDatePicker: UIDatePicker {
+
+  var hitTestDelegate: HitTestDelegate!
+  
+  override func becomeFirstResponder() -> Bool {
+    return super.becomeFirstResponder()
+  }
+  
+  override func canBecomeFirstResponder() -> Bool {
+    return super.canBecomeFirstResponder()
+  }
+  
+  override func hitTest(point: CGPoint, withEvent event: UIEvent?) -> UIView? {
+    hitTestDelegate.hitTestCalledForView(self)
+    //print("hitTest")
+    return super.hitTest(point, withEvent: event)
+  }
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
