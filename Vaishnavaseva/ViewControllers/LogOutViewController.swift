710ee3337700d77b04b727585465ed0de64cfdb7
Roman Slysh <monar.ua@gmail.com>
2015-11-01 18:52:58 +0200

    LogOut view updates.

--- Vaishnavaseva/ViewControllers/LogOutViewController.swift
+++ Vaishnavaseva/ViewControllers/LogOutViewController.swift
@@ -1,9 +1,15 @@
 import UIKit
 
 class LogOutViewController: BaseViewController {
+  @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var photo: UIImageView!
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    let me = NSUserDefaults.standardUserDefaults().objectForKey("me") as! NSDictionary
+    let sadhanaUser = Deserialiser().getSadhanaUser(me)
+    self.nameLabel.text = sadhanaUser.userName
+    self.photo.load(sadhanaUser.avatarUrl!)
     // Do any additional setup after loading the view, typically from a nib.
   }
 
