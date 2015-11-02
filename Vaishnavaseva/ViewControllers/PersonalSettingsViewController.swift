eb42bd73f7ae0fe3f55f568987f582f7c24bfe51
Evgenii Bondarev <putnik@ukr.net>
2015-11-02 19:25:52 +0200

    Implemented first version of UI for Settings screen; Added a segue for the Done button

--- Vaishnavaseva/ViewControllers/PersonalSettingsViewController.swift
+++ Vaishnavaseva/ViewControllers/PersonalSettingsViewController.swift
@@ -7,6 +7,10 @@ class PersonalSettingsViewController: BaseViewController {
     // Do any additional setup after loading the view, typically from a nib.
   }
   
+  @IBAction func onDone(sender: AnyObject) {
+    self.performSegueWithIdentifier("BackFromAnyToAny", sender: nil)
+  }
+
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
