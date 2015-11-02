a032c6a86a2db4da3b90c6e6a30ff7d672a923b0
Evgenii Bondarev <putnik@ukr.net>
2015-11-02 11:39:49 +0200

    Fixed a crash on Personal Settings Screen by adding custom VC, AppControllerState and AppControllerSegue for it

---
+++ Vaishnavaseva/AppController/Segues/MyToSettings.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+//@objc(ClassName) is needed to avoid having a module name in the class name for reflection, see BaseViewController.prepareForSegue()
+@objc(MyToSettings) class MyToSettings : AppControllerSegue
+  {
+  override func perform()
+    {
+    let destinationState = AppControllerStateInfo()
+    destinationState.viewController = self.visualSegue.destinationViewController
+    self.destinationSceneController = destinationState
+    
+    super.perform()
+    //Transfer any data between AppControllerStates and ViewControllers here
+    }
+  }
